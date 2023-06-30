0f37eb0e96be9ffbd90bfbe8b74027db85e853be
Lucas Levingston <lucaolevingston@outlook.com>
2023-06-30 15:06:46 -0300

    enviando alteracoes no projects.

--- src/components/ProjectsComponents.tsx
+++ src/components/ProjectsComponents.tsx
@@ -12,9 +12,23 @@ export default function ProjectsComponents(projeto: Project) {
       <h1 className="text-bold">{projeto.name}</h1>
       <br />
       <p>{projeto.sobre}</p>
-      <img src={projeto.image1} alt="" />
-      <img src={projeto.image2} alt="" />
-      <img src={projeto.image3} alt="" />
+      <div className="w-full h full rounded-2xl bg-center bg-cover duration-500">
+        <img
+          className="sm:w-auto sm:h-[280px]  mb-6 mx-auto"
+          src={projeto.image1}
+          alt="foto 1"
+        />
+        <img
+          className="sm:w-auto sm:h-[280px]  mb-6 mx-auto"
+          src={projeto.image2}
+          alt="foto 2"
+        />
+        <img
+          className="sm:w-auto sm:h-[280px]  mb-6 mx-auto"
+          src={projeto.image3}
+          alt="foto 3"
+        />
+      </div>
     </div>
   );
 }
