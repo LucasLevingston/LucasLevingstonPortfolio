b4b5cf4873ac831b2137817ac41875a557b7efef
Lucas Levingston <lucaolevingston@outlook.com>
2023-06-30 16:50:10 -0300

    imagens do projects adicionadas

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -12,17 +12,13 @@ const projetos: Project[] = [
     name: "CRM",
     sobre:
       "Este projeto está sendo desenvolvido no estágio. É um sistema de CRM, responsável pela integração dos dados obtidos pelos vendedores e armazenando os mesmos no banco de dados mongoDB com uma automação de envio de mensagens de texto para vendas, desde a entrada de dados de um cliente utilizando uma interface desenvolvida em React até a automação do envio de mensagens de WhatsApp. Javascript é a linguagem utilizada tanto no frontend quanto no backend, usando React e Node (com Express) respectivamente. Também foi utilizada a biblioteca Tailwind. Eu juntamente com minha equipe também entramos na área de Engenharia para desenvolvimento do projeto, utilizando diagrama de casos de uso, diagrama de classe, etc.",
-    image1: CRM1,
-    image2: CRM2,
-    image3: CRM3,
+    images: [CRM1, CRM2, CRM3],
   },
   {
     name: "Campo Minado",
     sobre:
       "Campo minado foi um projeto na linguagem Java de um jogo já existente, onde o objetivo do jogo é o usuário abrir todos os campos do jogo sem abrir nenhum campo que esteja minado, caso ele abra, irá perder o jogo. Para a abertura de campos, utilizei o padrão observer. Quando tem um campo que esteja minado, os campos vizinhos irão avisar que tem entre 1 e 4 campos minados dentre os seus vizinhos. Com isso, abrindo campo por campo, até completar o jogo. Na parte gráfica utilizei o Jbutton, onde cada campo representa um botão. Utilizei a biblioteca do JUnit para os testes da aplicação.",
-    image1: "",
-    image2: "",
-    image3: "",
+    images: [],
   },
 ];
 
@@ -30,7 +26,7 @@ export default function Projects() {
   return (
     <div className="flex flex-col sm:flex-row w-[30]">
       <Sidebar />
-      <div className="flex flex-col sm:max-w-[75%]  ml-auto mr-0 p-12 bg-aboutBgColor">
+      <div className="flex flex-col sm:max-w-[75%]  ml-auto mr-0 p-12 pb-0 bg-aboutBgColor">
         <Header />
         <a
           className="hover:bg-transparent font-bold text-[13px] bg-mainColor border-[3px] border-mainColor rounded-[5px] border-solid no-underline transition-[0.5s] m-[25px_0] p-[12px_10px] w-[150px] text-center flex"
@@ -50,9 +46,7 @@ export default function Projects() {
               key={projeto.name}
               name={projeto.name}
               sobre={projeto.sobre}
-              image1={projeto.image1}
-              image2={projeto.image2}
-              image3={projeto.image3}
+              images={projeto.images}
             />
           ))}
         </div>
