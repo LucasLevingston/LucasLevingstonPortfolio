5b087e094a1f9ce6f498bbfcb7770d6fda341596
LucasLevingston <LucasLevingston>
2023-07-14 17:00:48 -0300

    Writer corrigido e corrigido Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,73 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { AiFillHome } from "react-icons/ai";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+let colorHome: string = 'bg-mainColor';
+let colorSobre: string = 'bg-mainColor';
+let colorProjects: string = 'bg-mainColor';
+function corHome() {
+	colorHome = 'bg-transparent';
+	colorSobre = 'bg-mainColor';
+	colorProjects = 'bg-mainColor';
+}
+function corSobre() {
+	colorSobre = 'bg-transparent';
+	colorHome = 'bg-mainColor';
+	colorProjects = 'bg-mainColor';
+}
+function corProjects() {
+	colorProjects = 'bg-transparent';
+	colorSobre = 'bg-mainColor';
+	colorHome = 'bg-mainColor';
+}
+
+if (window.location.pathname === '/') {
+	colorHome = 'bg-transparent';
+	colorSobre = 'bg-mainColor';
+	colorProjects = 'bg-mainColor';
+}
+if (window.location.pathname === '/sobre') {
+	colorSobre = 'bg-transparent';
+	colorHome = 'bg-mainColor';
+	colorProjects = 'bg-mainColor';
+} else if (window.location.pathname === '/projetos') {
+	colorProjects = 'bg-transparent';
+	colorHome = 'bg-mainColor';
+	colorSobre = 'bg-mainColor';
+}
 
 export default function Navbar() {
-  return (
-    <nav className="">
-      <ul className="flex justify-center list-none text-center gap-3">
-        <li className="mx-0">
-          <Link
-            className="hover:bg-transparent flex-start   hover:text-mainTextColor bg-mainColor border-[2px] border-mainColor rounded-[3px] border-solid no-underline transition-[0.5s] m-[25px_0] p-[12px_12px] w-[80px] text-center flex content-between"
-            to="/"
-          >
-            {/* <AiFillHome className="text-xl mainTextColor" / */}
-            <span className="text-[16px]font-bold">Home</span>
-          </Link>
-        </li>
-        <li className="mx-0">
-          <Link
-            className="hover:bg-transparent font-bold  text-[16px] hover:text-mainTextColor bg-mainColor border-[2px] border-mainColor rounded-[5px] border-solid no-underline transition-[0.5s] m-[25px_0] p-[12px_14px] w-[80px] text-center flex"
-            to="/sobre"
-          >
-            Sobre
-          </Link>
-        </li>
-        <li className="mx-0">
-          <Link
-            className="hover:bg-transparent mx-auto font-bold text-[16px] hover:text-mainTextColor bg-mainColor border-[2px] border-mainColor rounded-[5px] border-solid no-underline transition-[0.5s] m-[25px_0] p-[12px_5px] w-[80px] text-center flex"
-            to="/projetos"
-          >
-            Projetos
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className="">
+			<ul className="flex justify-center list-none text-center gap-3">
+				<li className="mx-0">
+					<Link
+						className={`hover:bg-transparent font-bold  text-[16px] hover:text-mainTextColor ${colorSobre} border-[2px] border-mainColor rounded-[5px] border-solid no-underline transition-[0.5s] m-[25px_0] p-[12px_14px] w-[80px] text-center flex`}
+						to="/sobre"
+						onClick={corSobre}
+					>
+						Sobre
+					</Link>
+				</li>
+				<li className="mx-0">
+					<Link
+						className={`hover:bg-transparent flex-start text-center hover:text-mainTextColor ${colorHome} border-[2px] border-mainColor rounded-[3px] border-solid no-underline transition-[0.5s] m-[25px_0] p-[12px_12px] w-[80px] text-center flex content-between`}
+						to="/"
+						onClick={corHome}
+					>
+						{/* <AiFillHome className="text-xl mainTextColor" / */}
+						<span className="text-[16px]font-bold">Home</span>
+					</Link>
+				</li>
+				<li className="mx-0">
+					<Link
+						className={`hover:bg-transparent mx-auto font-bold text-[16px] hover:text- ${colorProjects} hover:text-mainTextColor border-[2px] border-mainColor rounded-[5px] border-solid no-underline transition-[0.5s] m-[25px_0] p-[12px_5px] w-[80px] text-center flex`}
+						to="/projetos"
+						onClick={corProjects}
+					>
+						Projetos
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
